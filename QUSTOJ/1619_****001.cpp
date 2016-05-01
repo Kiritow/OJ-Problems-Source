2fc9b9bc2ae0be6bd58951d4d3e00e290f96586d
KiritoTRw <3021577574@qq.com>
2016-05-01 18:43:32 +0800

    Create 1619_****001.cpp

---
+++ QUSTOJ/1619_****001.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    switch(a){
+    case 1:printf("Monday");break;
+    case 2:printf("Tuesday");break;
+    case 3:printf("Wednesday");break;
+    case 4:printf("Thursday");break;
+    case 5:printf("Friday");break;
+    case 6:printf("Saturday");break;
+    default:printf("Sunday");
+     
+     
+     
+    }
+return 0;
+ 
+}
