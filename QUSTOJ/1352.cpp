f0da649615a6d258703aa58242d7482bce69632d
Kirito <1362050620@qq.com>
2016-05-23 12:44:34 +0800

    Create 1352.cpp

---
+++ QUSTOJ/1352.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+//Written by Kiritow. 求最大公约数
+long gcd(long a,long b)
+{
+	if(a==0||b==0)
+	{
+		if(a+b==0)
+		{
+			return 0;
+		}
+		return (a==0)?b:a;
+	}
+
+	while(a!=b)
+	{
+		if(a>b)
+		{
+			a=a-b;
+		}
+		else
+		{
+			b=b-a;
+		}
+	}
+	return a;
+}
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    long s=gcd(a,b);
+    printf("%ld %ld\n",s,a*b/s);
+    return 0;
+}
