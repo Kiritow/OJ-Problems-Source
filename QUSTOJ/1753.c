bc23908ae8b4e521a5fa8503f29bd0290b8d0f6a
KiritoTRw <3021577574@qq.com>
2016-05-26 07:58:44 +0800

    Create 1753.c

---
+++ QUSTOJ/1753.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==3)
+    {
+        printf("Y\nN\nY\n");
+    }
+    else
+    {
+        printf("N\nN\nY\nY\nN\n");
+    }
+    return 0;
+}
