cbdde333e206af98cf4d99356cba056510272ee6
Kirito <1362050620@qq.com>
2016-10-24 16:55:00 +0800

    Create 1866.cpp

---
+++ QUSTOJ/1866.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+/// Why this online judge don't support C++11??? F***....
+/// #include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int x,z;
+    double y;
+    cin>>x>>y>>z;
+    cout<<pow(x,z)<<" "<<pow(y,z)<<endl;
+    return 0;
+}
