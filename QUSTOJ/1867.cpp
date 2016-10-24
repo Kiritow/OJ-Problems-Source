e8d029c4c355144bed0836aa3bcfd934ea0a21c3
Kirito <1362050620@qq.com>
2016-10-24 17:01:23 +0800

    Create 1867.cpp

---
+++ QUSTOJ/1867.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a,b,c,d,e,f;
+    cin>>a>>b>>c>>d>>e>>f;
+    cout<<"Num:"<<a<<endl;
+    cout<<"Sex:"<<b<<endl;
+    cout<<"Birth Date:"<<c<<" "<<d<<" "<<e<<endl;
+    cout<<"ID:"<<f<<endl;
+    return 0;
+}
