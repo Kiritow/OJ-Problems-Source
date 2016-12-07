5fa00bec61fbda3800dd7bfaf8c0fc94289b2932
Kirito <1362050620@qq.com>
2016-12-07 19:02:04 +0800

    Create 1907.cpp

---
+++ QUSTOJ/1907.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        cout<<a*b/2<<" "<<c*d<<endl;
+    }
+    return 0;
+}
