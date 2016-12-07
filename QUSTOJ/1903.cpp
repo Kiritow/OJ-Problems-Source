dc23499004844631546434227f62a2e81ec28b07
Kirito <1362050620@qq.com>
2016-12-07 18:50:13 +0800

    Create 1903.cpp

---
+++ QUSTOJ/1903.cpp
@@ -0,0 +1,41 @@
+/**
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+Mother Fuck to the one who made this problem!!!!
+*/
+
+/// I am a DOG ~
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+#define A1 int
+#define A2 n
+#define A3 string
+#define A4 a
+#define A5 b
+#define A6 cin
+#define A7 cout
+#define A8(x) while(x--)
+
+A1 main()
+{
+    A1 A2;
+    A3 A4,A5;
+    A6>>A2;
+    A8(A2)
+    {
+        A6>>A4>>A5;
+        A7<<(A1)A4.find(A5)<<endl;
+    }
+    return 0;
+}
